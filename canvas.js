db5d1b2a7b98befe6ee07e6ad9e0ae3c0ae98cd0
lmoreau21 <61840342+lmoreau21@users.noreply.github.com>
2023-01-23 08:12:21 -0600

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,24 +1,10 @@
 function setup() {
   createCanvas(800, 400);
+  background('lime');
 }
 
 function draw() {
-  shapeWidth = 300;
-  background(0);
-  noStroke()
-  
-  fill("yellow")
-  arc(width/4,height/2,shapeWidth,shapeWidth,-3*PI/4,-5*PI/4,PIE)
-  
-  fill("red")
-  rect(width/2+shapeWidth/6, height/2, shapeWidth,shapeWidth/2)
-  circle(width*3/4, height/2, shapeWidth)
-  
-  fill("white")
-  circle(width*3/4-shapeWidth/4, height/2, shapeWidth/3.5)
-  circle(width*3/4+shapeWidth/4, height/2, shapeWidth/3.5)
-  
-  fill("blue")
-  circle(width*3/4-shapeWidth/4, height/2, shapeWidth/6)
-  circle(width*3/4+shapeWidth/4, height/2, shapeWidth/6)
+  shapeWidth = 350;
+  circle(width/4, height/2, shapeWidth);
+  rect(width/2+(width/2-shapeWidth)/2,(width/2-shapeWidth)/2,shapeWidth)
 }
